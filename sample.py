f0a11110fc3f1a54fb5b4769ba8fc74fb3b84b6a
Wen <113942165+wen-coding@users.noreply.github.com>
2024-04-11 10:51:26 -0700

    A few cosmetic changes.

--- sample.py
+++ sample.py
@@ -26,14 +26,15 @@ def analyze_stakes(stakes, original_sum):
             low_stakes += 1
     return total, high_stakes, low_stakes
 
-def perform_simulation(original_stakes, sum_stakes):
+def perform_simulation(stakes):
     '''Performs simulation on stakes.'''
-    original_count = len(original_stakes)
+    sum_stakes = sum(stakes)
+    count_stakes = len(stakes)
     prob = np.array(original_stakes)/sum_stakes
     mins = {}
     maxs = {}
     states = {}
-    for i in range(original_count):
+    for i in range(count_stakes):
         states[i] = 0
     choices = []
     serving = []
@@ -42,7 +43,7 @@ def perform_simulation(original_stakes, sum_stakes):
         select_count = args.samples if len(choices) == 0 else rotate_number
         available = []
         available_prob = []
-        for i in range(original_count):
+        for i in range(count_stakes):
             if states[i] == 0:
                 available.append(i)
                 available_prob.append(prob[i])
@@ -76,15 +77,16 @@ parser.add_argument('--rounds', dest='rounds', default=1000, type=int,
 parser.add_argument('--rotation', dest='rotation', default=10, type=int,
                     help='Percentage of stake rotation on each round (default to 10)')
 args = parser.parse_args()
-stakes = read_stakes("./validators_stakes_epoch_600")
-sum_stakes = sum(stakes)
-_, original_highstake, original_lowstake = analyze_stakes(stakes, sum_stakes)
-print(f"total stake {sum_stakes} high_stakes (>1%) {original_highstake} "
+original_stakes = read_stakes("./validators_stakes_epoch_600")
+sum_original_stakes = sum(original_stakes)
+original_total, original_highstake, original_lowstake = analyze_stakes(
+    original_stakes, sum_original_stakes)
+print(f"total stake {sum_original_stakes} high_stakes (>1%) {original_highstake} "
       f"low_stakes (<0.01%) {original_lowstake}")
-print(f"random sampling {args.samples} out of {len(stakes)}, "
+print(f"random sampling {args.samples} out of {original_total}, "
       f"{args.rounds} rounds rotating {args.rotation}% stakes every round")
-range_mins, range_maxs = perform_simulation(stakes, sum_stakes)
-print(f"total {range_mins[0]*100/sum_stakes:2.2f}% to "
-      f"{range_maxs[0]*100/sum_stakes:2.2f}% high_stakes(>1%) "
+range_mins, range_maxs = perform_simulation(original_stakes)
+print(f"total {range_mins[0]*100/sum_original_stakes:2.2f}% to "
+      f"{range_maxs[0]*100/sum_original_stakes:2.2f}% high_stakes(>1%) "
       f"{range_mins[1]} to {range_maxs[1]} low_stakes(<0.01%%) "
       f"{range_mins[2]} to {range_maxs[2]}")
